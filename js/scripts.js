806e4e3841b1b56953a9a743abba497fe0b035d3
Cassandra Culkins <cassy.culkins@gmail.com>
2017-04-16 14:09:02 -0700

    add header and home content

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
-window.onload = function() {
-    if (window.jQuery) {
-        // jQuery is loaded
-        alert("Yeah!");
-    } else {
-        // jQuery is not loaded
-        alert("Doesn't Work");
-    }
-}
+// window.onload = function() {
+//     if (window.jQuery) {
+//         // jQuery is loaded
+//         alert("Yeah!");
+//     } else {
+//         // jQuery is not loaded
+//         alert("Doesn't Work");
+//     }
+// }
