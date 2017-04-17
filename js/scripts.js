ff5ab2b7b2cbb4fe745951eb321426b1e64eeb8d
Cassandra Culkins <cassy.culkins@gmail.com>
2017-04-16 22:10:07 -0700

    add jquery lightbox to illustration page

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,34 @@
-// window.onload = function() {
-//     if (window.jQuery) {
-//         // jQuery is loaded
-//         alert("Yeah!");
-//     } else {
-//         // jQuery is not loaded
-//         alert("Doesn't Work");
-//     }
-// }
+// $("#pagePort").load("illustration.html", function(){
+//   $.getScript("js/scripts.js", function() {
+//       if (currentPage.init) {
+//           currentPage.init();
+//       }
+//   });
+// });
+$(document).ready(function(){
+
+var jqueryOverlay = $('<div id="overlay"></div>');
+var jqueryImage = $("<img>");
+var jqueryCaption = $("<p></p>");
+
+jqueryOverlay.append(jqueryImage);
+jqueryOverlay.append(jqueryCaption);
+
+$("body").append(jqueryOverlay);
+
+$("#imageGallery a").click(function(event) {
+ event.preventDefault();
+ var imageLocation = $(this).attr("href");
+ jqueryImage.attr("src", imageLocation);
+
+ jqueryOverlay.show();
+
+ var captionText = $(this).children("img").attr("alt");
+  jqueryCaption.text(captionText);
+});
+
+jqueryOverlay.click(function() {
+  jqueryOverlay.hide();
+});
+
+});
